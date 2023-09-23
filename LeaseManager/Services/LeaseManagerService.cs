e2b786aef471d2a8de645f5a1f19aa7d8e50118f
Miguel Rocha <miguelasrocha1@gmail.com>
2023-09-23 23:51:57 +0100

    Updates LeaseManager to read the config file

--- LeaseManager/Services/LeaseManagerService.cs
+++ LeaseManager/Services/LeaseManagerService.cs
@@ -2,22 +2,31 @@ namespace LeaseManager.Services;
 
 using Grpc.Core;
 using GrpcDADTKV;
+using Grpc.Net.Client;
+using System.Diagnostics;
 
 public class LeaseManagerService : DadTkvLeaseManagerService.DadTkvLeaseManagerServiceBase
 {
-    Paxos paxos = new Paxos();
+    const string clientScriptFilename = "../configuration_sample";
+    int timeSlot;
+    int nOfSlots;
+    // const string clientScriptFilename = "C:/Users/migas/Repos/dad-project/configuration_sample";
     List<RequestLeaseRequest> requests = new List<RequestLeaseRequest>();
     Boolean isLeader = false;
+    string node;
+    HashSet<string> nodes;
     int promised = 0; // FIXME: use concurrency bullet proof
 
     public LeaseManagerService(string node, HashSet<string> nodes)
     {
+        ProcessConfigurationFile();
+
         void BroadcastPrepareRequest()
         {
             foreach (string node in nodes)
             {
                 using var channel = GrpcChannel.ForAddress("http://localhost:5000");
-                var client = new DADTKV.DADTKVClient(channel);
+                var client = new DadTkvLeaseManagerService.DadTkvLeaseManagerServiceClient(channel);
                 client.Prepare(new PrepareRequest());
             }
         }
@@ -31,7 +40,41 @@ public class LeaseManagerService : DadTkvLeaseManagerService.DadTkvLeaseManagerS
         }
     }
 
-    public void DefineLeader()
+    void ProcessConfigurationFile()
+    {
+        IEnumerator<string> lines = File.ReadLines(clientScriptFilename).GetEnumerator();
+        while (lines.MoveNext())
+        {
+            string line = lines.Current;
+            if (!line.StartsWith('#')) // not a comment
+            {
+                string[] split = line.Split(' ');
+                string firstToken = split[1];
+                /*
+                if (line.StartsWith('P'))
+                {
+                    string type = split[2];
+                    if (type == "L")
+                    {
+                        string name = firstToken;
+                        int href = int.Parse(split[3]);
+                        // TODO: create a new lease manager
+                    }
+                }
+                */
+                if(line.StartsWith('D'))
+                {
+                    timeSlot = int.Parse(firstToken);
+                }
+                else if(line.StartsWith('S'))
+                {
+                    nOfSlots = int.Parse(firstToken);
+                }
+            }
+        }
+    }
+
+    void DefineLeader()
     {
         IEnumerable<int> nodesCastedToInt = nodes.Cast<int>(); // cast the nodes to int
         int min = nodesCastedToInt.Min(); // get the min value
@@ -46,6 +89,8 @@ public class LeaseManagerService : DadTkvLeaseManagerService.DadTkvLeaseManagerS
         // maybe use a pool request
         // maybe put this thread to sleep and wake it up when the paxos instance is executed
 
+        requests.Add(request);
+
         return Task.FromResult(new RequestLeaseReply{});
     }
 
@@ -57,34 +102,31 @@ public class LeaseManagerService : DadTkvLeaseManagerService.DadTkvLeaseManagerS
             foreach (string node in nodes)
             {
                 using var channel = GrpcChannel.ForAddress("http://localhost:5000");
-                var client = new DADTKV.DADTKVClient(channel);
+                var client = new DadTkvLeaseManagerService.DadTkvLeaseManagerServiceClient(channel);
                 client.Accept(new AcceptRequest());
             }
         }
 
-        Console.WriteLine($"Request: {request.TransactionManager}");
         promised++; // one more promise accepted
         if (promised > nodes.Count / 2) // majority
         {
             BroadcastAcceptRequest();
         }
+        return Task.FromResult(new PrepareReply{});
     }
 
-    public override Task<AcceptReply> Promise(PromiseRequest request, ServerCallContext context)
+    public override Task<PromiseReply> Promise(PromiseRequest request, ServerCallContext context)
     {
-        Console.WriteLine($"Request: {request.TransactionManager}");
-        return Task.FromResult(new RequestLeaseReply{});
+        return Task.FromResult(new PromiseReply{});
     }
 
     public override Task<AcceptReply> Accept(AcceptRequest request, ServerCallContext context)
     {
-        Console.WriteLine($"Request: {request.TransactionManager}");
-        return Task.FromResult(new RequestLeaseReply{});
+        return Task.FromResult(new AcceptReply{});
     }
 
-    public override Task<AcceptReply> Accepted(AcceptedRequest request, ServerCallContext context)
+    public override Task<AcceptedReply> Accepted(AcceptedRequest request, ServerCallContext context)
     {
-        Console.WriteLine($"Request: {request.TransactionManager}");
-        return Task.FromResult(new RequestLeaseReply{});
+        return Task.FromResult(new AcceptedReply{});
     }
 }
