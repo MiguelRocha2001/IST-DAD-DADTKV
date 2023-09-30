cebe7d9afd989356d96660ebebb612df23b8b4a7
Miguel Rocha <miguelasrocha1@gmail.com>
2023-09-30 18:43:47 +0100

    LeaseManagerTest project added

---
+++ LeaseManagerTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using Grpc.Net.Client;
+using GrpcDADTKVLease;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:6002");
+var client = new DadTkvLeaseManagerService.DadTkvLeaseManagerServiceClient(channel);
+RequestLeaseRequest request = new RequestLeaseRequest();
+request.TransactionManager = "localhost:5000";
+client.RequestLease(request);
