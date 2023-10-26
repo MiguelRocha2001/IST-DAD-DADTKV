0b92be8bf783828559a5678143c08d548e8cd47e
harbots <haris.botsas89@gmail.com>
2023-10-26 23:42:19 +0100

    Fixes client program script

--- Client/Program.cs
+++ Client/Program.cs
@@ -26,7 +26,7 @@ class Program
         using var channel = GrpcChannel.ForAddress(transactionManagerServer); // grpc channel
         var client = new DADTKV.DADTKVClient(channel); // grpc client
 
-        List<Tuple<CommandType, string>> CommandList = new List<Tuple<CommandType, string>>();
+        List<Tuple<CommandType, string[]>> CommandList = new List<Tuple<CommandType, string[]>>();
 
         // for some reason, running in debug mode, the client script file is not found (he assumes a different path, not sure why)
         IEnumerator<string> lines = File.ReadLines(clientScriptFilename).GetEnumerator();
@@ -39,11 +39,15 @@ class Program
             {
                 if (line.StartsWith('W')) // Wait cmd
                 {
-                    CommandList.Add(Tuple.Create(CommandType.Wait, line.Split(' ')[1]));
+                    CommandList.Add(Tuple.Create(CommandType.Wait, new[] { line.Split(' ')[1] }));
                 }
                 else if (line.StartsWith('T'))
                 {
-                    CommandList.Add(Tuple.Create(CommandType.Transaction, line));
+                    string[] transaction = line.Split(' ').Skip(1).ToArray();
+                    string CleanInput(string input) => input.Replace("<", "").Replace(">", "").Replace("\"", "").Replace("(", "").Replace(")", "");
+
+                    // tuple of the form (Transaction, ["a-key-name,another-key-name","name1,10,name2,20"])
+                    CommandList.Add(Tuple.Create(CommandType.Transaction, new[] { CleanInput(transaction[0]), CleanInput(transaction[1]) }));
                 }
                 else
                 {
@@ -54,36 +58,27 @@ class Program
 
         while (true)
         {
-            foreach (Tuple<CommandType, string> CommandPair in CommandList)
+            foreach (Tuple<CommandType, string[]> CommandPair in CommandList)
             {
                 switch (CommandPair.Item1)
                 {
                     case CommandType.Transaction:
-                        string[] transaction = CommandPair.Item2.Split(' ').Skip(1).ToArray();
-                        string CleanPar(string input) => input.Replace("(", "").Replace(")", "").Replace("\"", "");
-                        string[] readSet = CleanPar(transaction[0]).Split(',').ToArray();
 
-                        foreach (string i in readSet)
-                            Console.WriteLine(i);
-                        string[] writeSet = transaction[1].Split('>').ToArray();
-                        writeSet = writeSet.Take(writeSet.Length - 1).ToArray(); // removes last element (invalid)
+                        string[] readSet = CommandPair.Item2[0].Split(',');
+                        string[] writeSet = CommandPair.Item2[1].Split(',');
 
                         TxSubmitRequest request = new TxSubmitRequest(); // GRPC request
                         request.Client = "client1";
                         request.Reads.Add(readSet); // inserts read operations in the request
 
-                        string CleanInput(string input) => input.Replace("<", "").Replace(">", "").Replace("\"", "").Replace("(", "").Replace(")", "");
-
-                        foreach (string writeOper in writeSet) // inserts write operations in the request
+                        for (int i = 0; i < writeSet.Length-1; i+=2)
                         {
-                            string writeOperAux = writeOper;
-                            if (writeOperAux.StartsWith(',')) writeOperAux = writeOperAux.Substring(1); // removes first comma
-
                             DadInt dadInt = new DadInt();
-                            dadInt.Key = CleanInput(writeOperAux.Split(',')[0]);
-                            dadInt.Value = long.Parse(CleanInput(writeOperAux.Split(',')[1]));
+                            dadInt.Key = writeSet[i];
+                            dadInt.Value = int.Parse(writeSet[i+1]);
                             request.Writes.Add(dadInt);
                         }
+
                         // Console.WriteLine("Sending transaction...");
                         // TxSubmitReply reply = await Task.FromResult(client.TxSubmit(request)); // sends the request to the server (transaction manager)
                         // Console.WriteLine("Transaction result:");
@@ -94,7 +89,7 @@ class Program
                         Console.WriteLine("\n");
                         break;
                     case CommandType.Wait:
-                        int waitTime = int.Parse(CommandPair.Item2);
+                        int waitTime = int.Parse(CommandPair.Item2[0]);
                         Console.WriteLine("Waiting " + waitTime + "ms");
                         Task.Delay(waitTime).Wait();
                         Console.WriteLine("Waited " + waitTime + "ms");
